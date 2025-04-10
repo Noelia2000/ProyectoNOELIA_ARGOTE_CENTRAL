ba0177882ca7f2e19ed0e686d6985934a8abc5b3
Noe <noelia2000vane@gmail.com>
2025-04-09 22:32:41 -0400

    recover users

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import './index.css'
+import App from './App'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store'
+
+
+ReactDOM.createRoot(document.getElementById('root')as HTMLAnchorElement).render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
 )
