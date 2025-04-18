fdb2e84c6f7f6ac3b4f48a0ed7cdbc12256fbf29
Noe <noelia2000vane@gmail.com>
2025-04-18 00:02:03 -0400

    Retorno de las funciones

--- Curso_JavaScript/01-fundamentos/assets/js/funciones.js
+++ Curso_JavaScript/01-fundamentos/assets/js/funciones.js
@@ -1,6 +1,10 @@
 function saluar(nombre) {
-    console.log(arguments);
-    console.log('Hola '+nombre);
+    //console.log(arguments);
+    //console.log('Hola '+nombre);
+return [1,2];
+//esto nunca se va a ejecutar
+console.log('Soy un codigo que esta despues de un return');
+
 }
 const saludar2 = function (nombre) {
   console.log('Hola'+nombre);
@@ -13,8 +17,34 @@ const saludarFlecha=()=>{
 const saludarFlecha2=(nombre)=>{
     console.log('Hola '+nombre);
 }
-saluar('Noelia',40,true,'CostaRica');
-//saludar2('Noelia');
-saludarFlecha();
-saludarFlecha2('Ana');
 
+const retornoDeDaludar= saluar('Noelia',40,true,'CostaRica');
+
+//saludar2('Noelia');
+//saludarFlecha();
+//saludarFlecha2('Ana');
+
+function sumar(a,b){
+    return a+b;
+}
+
+//const sumar2=(a,b)=>{
+    //return a+b;
+//}
+
+const sumar2=(a,b)=>a+b; //return implicito
+
+
+
+function getAleatorio(){
+    return Math.random();
+} 
+
+// en una funcion de flecha, que no tenga llaves{
+//getAleatorio2()
+
+const getAleatorio2=()=>Math.random(); //return implicito
+
+
+
+console.log(getAleatorio()); 
