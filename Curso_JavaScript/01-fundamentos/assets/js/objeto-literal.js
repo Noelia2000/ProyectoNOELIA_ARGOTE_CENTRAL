ce05f54585cbf1ee959620fcc38d8548316bb5c3
Noe <noelia2000vane@gmail.com>
2025-04-17 22:19:16 -0400

    Mas detalles sobre los objetos literales

--- Curso_JavaScript/01-fundamentos/assets/js/objeto-literal.js
+++ Curso_JavaScript/01-fundamentos/assets/js/objeto-literal.js
@@ -1,5 +1,5 @@
 
-let personaje={
+const personaje={
     nombre:'Tony Stark',
     codeName:'Ironman',
     vivo: false,
@@ -38,3 +38,33 @@ console.log('Ultimo Traje',personaje.trajes[personaje.trajes.length-1]); //Hulkb
 const x ='vivo';
 console.log('Vivo',personaje[x]); //false
 console.log('Ultima Pelicula',personaje['ultima-pelicula']); //Infinity War
+
+
+// Mas detalles
+
+delete personaje.edad;
+
+
+console.log(personaje);
+
+
+personaje.casado=true; //agregar una propiedad al objeto
+
+const entriesPares=Object.entries(personaje);
+console.log(entriesPares);
+//personaje=true;
+
+
+Object.freeze(personaje); //no se puede modificar el objeto
+
+
+
+personaje.dinero=1000000; //no se puede agregar una propiedad al objeto
+personaje.casado=false; //no se puede modificar una propiedad del objeto
+personaje.direccion.ubicacion='Costa Rica'; //no se puede modificar una propiedad del objeto
+
+console.log(personaje);
+
+const propiedades=Object.getOwnPropertyNames(personaje);
+const valores=Object.values(personaje);
+console.log({propiedades,valores});
