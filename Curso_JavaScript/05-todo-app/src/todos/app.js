5e925465af0fd80ef8f4c90fd86eb1d5d9d2d53e
Noe <noelia2000vane@gmail.com>
2025-04-24 09:49:11 -0400

    Renderizar listado de Todos

--- Curso_JavaScript/05-todo-app/src/todos/app.js
+++ Curso_JavaScript/05-todo-app/src/todos/app.js
@@ -1,10 +1,29 @@
- import html from './app.html?raw';
+ import todoStore from '../store/todo.store';
+import html from './app.html?raw';
+import { renderTodos } from './use-cases';
+
+
+const ElementIDs={
+
+    TodoList:'.todo-list',
+}
+
+
  /**
   * 
   * @param {String} elementId 
   */
 
  export const App =(elementId)=>{
+
+    const displayTodos=()=>{
+
+       const todos = todoStore.getTodo( todoStore.getCurrentFilter); 
+       renderTodos(ElementIDs.TodoList, todos);
+
+
+    }
+
     
 
 // cuando la funcion App() se llama
