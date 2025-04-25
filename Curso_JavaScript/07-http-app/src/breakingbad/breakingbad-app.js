73b353eaed29e2012026919db17205d34df83324
Noe <noelia2000vane@gmail.com>
2025-04-25 18:51:03 -0400

    BreakingBad component

---
+++ Curso_JavaScript/07-http-app/src/breakingbad/breakingbad-app.js
@@ -0,0 +1,15 @@
+import { Title } from "@tremor/react";
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const BreakingbadApp=(element)=>{
+
+    document.querySelector('#app-title').innerHTML= 'Breakingbad App';
+
+    element.innerHTML='Loading...';
+   
+
+
+}
