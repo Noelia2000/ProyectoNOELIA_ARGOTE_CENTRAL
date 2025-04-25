93c6567abd343f413cf1c994e1155ae2836ed726
Noe <noelia2000vane@gmail.com>
2025-04-25 13:02:59 -0400

    Ejemplo de función generadora

--- Curso_JavaScript/06-advanced/src/concepts/09-generators.js
+++ Curso_JavaScript/06-advanced/src/concepts/09-generators.js
@@ -6,17 +6,32 @@
  */
 export const generatorFunctionsComponent =(element)=>{
 
-const myGenerator = myFirstGeneratorFuncion();
+// const myGenerator = myFirstGeneratorFuncion();
+// console.log(myGenerator.next());
 
-console.log(myGenerator.next());
-console.log(myGenerator.next());
-console.log(myGenerator.next());
-console.log(myGenerator.next());
-console.log(myGenerator.next());
-console.log(myGenerator.next());
+const genId = idGenerator();
 
+const button = document.createElement('button');
+button.innerText=' Click me';
+element.append(button);
 
+const renderButton = ()=>{
+    const{value}= genId.next();
+    button.innerText=`Click ${value}`;
 }
+  
+button.addEventListener('click',renderButton);
+}
+
+
+function* idGenerator(){
+    let currentId=0;
+    while (true) {
+        yield ++ currentId;
+        
+    }
+}
+
 
 function* myFirstGeneratorFuncion(){
 
