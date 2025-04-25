9a64c1a17d0dd1be16fb4fbcd2ff61257451b36b
Noe <noelia2000vane@gmail.com>
2025-04-25 00:25:46 -0400

    Utilizar una promesa

--- Curso_JavaScript/06-advanced/src/concepts/03-promises.js
+++ Curso_JavaScript/06-advanced/src/concepts/03-promises.js
@@ -6,6 +6,21 @@ import { heroes } from "../data/heroes"
  */
 export const promiseComponent =(element)=>{
 
+    const renderHero=(hero)=>{
+        element.innerHTML=hero.name;
+    }
+
+    const renderError=(error)=>{
+        element.innerHTML= `
+        <h1>Error:</h1>
+        <h3>${error}</h3>`
+    }
+
+
+    const id1='5d86371f9f80b591f499df32';
+    findHero(id1)
+    .then(renderHero)
+    .catch(renderError);
 
 
 }
@@ -16,7 +31,7 @@ export const promiseComponent =(element)=>{
  */
 const findHero=(id)=>{
 return new Promise((resolver, reject)=>{
-    
+
     const hero = heroes.find(hero=> hero.id=== id);
 
     if (hero){
