169a0175c3f222956157de336c790ae400b8f59f
Noe <noelia2000vane@gmail.com>
2025-04-25 00:39:18 -0400

    Promise Hell

--- Curso_JavaScript/06-advanced/src/concepts/03-promises.js
+++ Curso_JavaScript/06-advanced/src/concepts/03-promises.js
@@ -9,6 +9,15 @@ export const promiseComponent =(element)=>{
     const renderHero=(hero)=>{
         element.innerHTML=hero.name;
     }
+const renderTwoHeroes=(hero1,hero2)=>{
+    element.innerHTML=`
+    <h3> ${hero1.name}</h3>
+    <h3> ${hero2.name}</h3>
+    `;
+}
+
+
+
 
     const renderError=(error)=>{
         element.innerHTML= `
@@ -18,8 +27,26 @@ export const promiseComponent =(element)=>{
 
 
     const id1='5d86371f9f80b591f499df32';
+    const id2='5d86371f233c9f2425f16916';
+
+   
+
+
     findHero(id1)
-    .then(renderHero)
+    .then((hero1)=>{
+
+       
+
+    findHero(id2)
+    .then(hero2=>{
+        renderTwoHeroes(hero1,hero2)
+
+    })
+
+    .catch (renderError);
+
+
+    })
     .catch(renderError);
 
 
