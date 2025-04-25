b12ad6726f7632229b83a4cfbfc95e0bd8cbbc20
Noe <noelia2000vane@gmail.com>
2025-04-25 11:03:28 -0400

    Errores en Async-Await

--- Curso_JavaScript/06-advanced/src/concepts/06-async-await.js
+++ Curso_JavaScript/06-advanced/src/concepts/06-async-await.js
@@ -10,11 +10,15 @@ export const asyncAwaitComponent= async(element)=>{
     const id1='5d86371fd55e2e2a30fe1ccb1';
     const id2='5d86371fd55e2e2a30fe1ccb2';
 
+    try{
     const hero1= await findHero(id1);
     const hero2= await findHero(id2);
 
 
     element.innerHTML=`${hero1.name} / ${hero2.name}`;
+    }catch(error){
+        element.innerHTML=error;
+    }
 
 
 
