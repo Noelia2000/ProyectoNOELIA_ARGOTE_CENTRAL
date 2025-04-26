f09fd31efd927f8e112b3aafce725dd347d1093c
Noe <noelia2000vane@gmail.com>
2025-04-26 16:52:53 -0400

    Botones - Next, Prev y página actual

--- Curso_JavaScript/07-http-app/src/users/use-cases/users-app.js
+++ Curso_JavaScript/07-http-app/src/users/use-cases/users-app.js
@@ -1,3 +1,4 @@
+import { renderButtons } from "../presentation/render-buttons/render-buttons";
 import { renderTable } from "../presentation/render-table/render-table";
 import usersStore from "../store/users-store";
 
@@ -12,4 +13,8 @@ export const UsersApp = async(element)=>{
     await usersStore.loadNextPage();
     element.innerHTML='';
    renderTable(element);
+
+
+   renderButtons(element);
+
 }
