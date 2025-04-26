26afd1b09bd9411c95bb2cf79713432c9d86949f
Noe <noelia2000vane@gmail.com>
2025-04-26 15:58:18 -0400

    Actualizar nuestro Store

--- Curso_JavaScript/07-http-app/src/users/store/users-store.js
+++ Curso_JavaScript/07-http-app/src/users/store/users-store.js
@@ -8,8 +8,12 @@ const state ={
 }
 
 const loadNextPage = async()=>{
-    await loadUsersByPage(state.currentPage +1);
+   const users= await loadUsersByPage(state.currentPage +1);
+ 
+   if (users.length===0)return;
 
+   state.currentPage+=1;
+   state.users=users;
 }
 
 const loadPreviousPage= async()=>{
@@ -38,6 +42,6 @@ export default{
      onUserChanged,
      reloadPage,
 
-     getUser:()=>[...state.users],
+     getUsers:()=>[...state.users],
      getCurrentPage:()=> state.currentPage,
 }
