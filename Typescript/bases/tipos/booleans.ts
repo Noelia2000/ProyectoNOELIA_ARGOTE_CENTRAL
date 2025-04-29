433596263f43e7b797361fc9d119a3b60cd35705
Noe <noelia2000vane@gmail.com>
2025-04-28 23:16:01 -0400

    Booleans - Booleanos

---
+++ Typescript/bases/tipos/booleans.ts
@@ -0,0 +1,13 @@
+
+// funcion anonima autoinnvocada
+(()=>{
+
+
+let isSuperman: boolean= true;
+let isBatman: boolean= false;
+
+isSuperman=(isBatman)? true:false;
+console.log({isSuperman})
+
+
+})() 
