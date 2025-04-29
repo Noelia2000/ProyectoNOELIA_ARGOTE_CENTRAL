a59fa05fa0108c7c41b96a5bd11b970096cac94d
Noe <noelia2000vane@gmail.com>
2025-04-29 10:56:13 -0400

    Strings - Cadenas de caracteres

---
+++ Typescript/bases/tipos/strings.ts
@@ -0,0 +1,12 @@
+(()=>{
+// tres formas de definir string
+const batman: string='Batman';
+const linternaVerde:string="Linterna Verde";
+const volcanNegro:string=`Heroe: Volcan Negro`;
+
+
+console.log( `I'm ${batman}`);
+console.log(batman.toUpperCase());
+
+console.log(batman[10]?.toUpperCase() || 'No esta presente')
+})()
