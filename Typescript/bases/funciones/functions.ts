f7f7871de8c784a6c07c47d7184a5cdc306b00f5
Noe <noelia2000vane@gmail.com>
2025-04-29 22:22:09 -0400

    Funciones básicas

---
+++ Typescript/bases/funciones/functions.ts
@@ -0,0 +1,21 @@
+(()=>{
+
+
+    const hero: string='Flash';
+
+    function returnName(): string{
+        return hero;
+
+    }
+
+    const activateBatisognal=(): string =>{
+        return 'Batiseñal activada!';
+    }
+
+    console.log(typeof activateBatisognal);
+
+    const heroName= returnName();
+
+
+
+})()
