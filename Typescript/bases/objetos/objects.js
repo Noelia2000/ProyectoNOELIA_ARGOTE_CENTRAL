a8dd3b7e796b53c64debd3f1c84cb0b285e9d144
Noe <noelia2000vane@gmail.com>
2025-04-30 23:40:17 -0400

    Problema con la definición en línea

--- Typescript/bases/objetos/objects.js
+++ Typescript/bases/objetos/objects.js
@@ -1,25 +1,38 @@
 "use strict";
+// (()=>{
+// //?Objetos con tipos especificos
+//     let flash:{name:string,age?:number,powers:string[], getName?:()=>string} ={
+//         name: 'Barry Allen',
+//         age:24,
+//         powers: ['Super velcidad', 'Viajar en el tiempo']
+//     }
+//     flash ={
+//         name:'Clark Kent',
+//         powers:['Super fuerza'],
+//         getName(){
+//        return this.name;
+//     }
+// }
+//     // flash = {
+//     //     name: 'Clark Kent',
+//     //     age:60,
+//     //     powers: ['Super fuerza'],
+//     //     getNombre(){
+//     //         return this.name;
+//     //     }
+//     // }
+//     console.log(flash.getName())
+// })()
 (() => {
-    //?Objetos con tipos especificos
+    //?definicion en linea
     let flash = {
         name: 'Barry Allen',
         age: 24,
         powers: ['Super velcidad', 'Viajar en el tiempo']
     };
-    flash = {
+    let superman = {
         name: 'Clark Kent',
-        powers: ['Super fuerza'],
-        getName() {
-            return this.name;
-        }
+        age: 24,
+        powers: ['Super velcidad']
     };
-    // flash = {
-    //     name: 'Clark Kent',
-    //     age:60,
-    //     powers: ['Super fuerza'],
-    //     getNombre(){
-    //         return this.name;
-    //     }
-    // }
-    console.log(flash.getName());
 })();
--- Typescript/bases/objetos/objects.ts
+++ Typescript/bases/objetos/objects.ts
@@ -1,36 +1,56 @@
+// (()=>{
+// //?Objetos con tipos especificos
+//     let flash:{name:string,age?:number,powers:string[], getName?:()=>string} ={
+//         name: 'Barry Allen',
+//         age:24,
+//         powers: ['Super velcidad', 'Viajar en el tiempo']
+//     }
+
+//     flash ={
+//         name:'Clark Kent',
+//         powers:['Super fuerza'],
+
+//         getName(){
+
+//        return this.name;
+//     }
+// }
+
+//     // flash = {
+//     //     name: 'Clark Kent',
+//     //     age:60,
+//     //     powers: ['Super fuerza'],
+//     //     getNombre(){
+
+//     //         return this.name;
+
+//     //     }
+//     // }
+
+//     console.log(flash.getName())
+
+
+// })()
+
+
+
 (()=>{
-//?Objetos con tipos especificos
-    let flash:{name:string,age?:number,powers:string[], getName?:()=>string} ={
-        name: 'Barry Allen',
-        age:24,
-        powers: ['Super velcidad', 'Viajar en el tiempo']
-    }
+    //?definicion en linea
+        let flash:{name:string,age?:number,powers:string[], getName?:()=>string} ={
+            name: 'Barry Allen',
+            age:24,
+            powers: ['Super velcidad', 'Viajar en el tiempo']
+        }
 
-    flash ={
-        name:'Clark Kent',
-        powers:['Super fuerza'],
-
-        getName(){
-
-       return this.name;
-    }
-}
-
-    // flash = {
-    //     name: 'Clark Kent',
-    //     age:60,
-    //     powers: ['Super fuerza'],
-    //     getNombre(){
-
-    //         return this.name;
-
-    //     }
-    // }
-
-    console.log(flash.getName())
-
-
-})()
-
-
+        let superman:{name:string,age?:number,powers:string[], getName?:()=>string} ={
+            name: 'Clark Kent',
+            age:24,
+            powers: ['Super velcidad']
+        }
+    
+    
+        
+    
+    })()
+    
 
