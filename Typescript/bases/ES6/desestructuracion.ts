19415c4a8999c04cdeb7e147d22ede1e6745e10b
Noe <noelia2000vane@gmail.com>
2025-05-01 16:50:43 -0400

    Desestructuración de Objetos

---
+++ Typescript/bases/ES6/desestructuracion.ts
@@ -0,0 +1,30 @@
+(()=>{
+    type Avengers={
+
+        nick: string;
+        ironman: string;
+        vision: string;
+        activo: boolean;
+        poder: number;
+
+    }
+
+const avengers: Avengers={
+    nick:'Samuel L. Jackson',
+    ironman:'Roberrt Downey Jr.',
+    vision:'Paul Bettany',
+    activo:true,
+    poder:1500.123123
+}
+// const {poder, vision}= avengers
+// console.log(poder.toFixed(2), vision.toUpperCase())
+
+const printAvenger=( {ironman,...resto}:Avengers)=>{
+
+  
+
+     console.log(ironman, resto);
+}
+printAvenger( avengers);
+})()
+
