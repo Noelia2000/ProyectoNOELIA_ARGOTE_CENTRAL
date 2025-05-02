45897f89778385085b3dee4e693400c150e90743
Noe <noelia2000vane@gmail.com>
2025-05-02 00:37:36 -0400

    Definición de una clase básica en TypeScript

---
+++ Typescript/bases/classes/basica.ts
@@ -0,0 +1,20 @@
+(()=>{
+ class Avenger{
+
+    //private significa de que solo voy a tener acceso a esta propiedad dentro  de la clase
+    private name:string;
+    private team: string;
+    public realName: string;
+    static avgAge:number=35;
+    constructor(name:string, team:string, realName?:string){
+        this.name=name;
+        this.team=team;
+        this.realName=realName;
+    }
+ }
+
+ const antman : Avenger=new Avenger('Antman','Capitan');
+ console.log(antman)
+// console.log(Avenger.avgAge)
+
+})()
