ce92b1e8134fc9416a801e90103011deb23a25d2
Noe <noelia2000vane@gmail.com>
2025-05-02 10:52:01 -0400

    Gets y Sets

--- Typescript/bases/classes/extends.ts
+++ Typescript/bases/classes/extends.ts
@@ -3,14 +3,14 @@
     class Avenger{
         constructor(
             public name:string,
-            public reaName:string,
+            public realName:string,
         ){
 
             console.log('Constructor Avenger llamado!')
 
         }
         protected getFullname(){
-            return `${this.name} ${this.reaName}`
+            return `${this.name} ${this.realName}`
         }
     }
 
@@ -25,6 +25,25 @@
             console.log('Constructor Xmen llamado');
         }
 
+
+        // Gets y sets son un metodo
+        // el get regresa 
+// el Gets no recive nigun argumento 
+
+        get fullName(){
+            return`${this.name}- ${this.realName}`;
+        }
+// el sets si recibe argumentos 
+        // un sets no regresa nada 
+        set fullName(name:string){
+            if (name.length<3){
+                throw new Error('El nombre debe de ser mayor de 3 letras')
+            }
+        this.name=name;
+
+        }
+
+
         getFullnameDesdeXmen(){
             console.log(super.getFullname())
         }
@@ -32,8 +51,11 @@
 
     const wolverine= new Xmen('Wolverine','Logan', true)
 
-    console.log(wolverine);
-    wolverine.getFullnameDesdeXmen();
+    console.log(wolverine.fullName);
+
+    wolverine.fullName='Noelia'
+    console.log(wolverine.fullName);
+    // wolverine.getFullnameDesdeXmen();
 
 
 
--- Typescript/bases/main.js
+++ Typescript/bases/main.js
@@ -17,13 +17,13 @@
 })();
 (() => {
     class Avenger {
-        constructor(name, reaName) {
+        constructor(name, realName) {
             this.name = name;
-            this.reaName = reaName;
+            this.realName = realName;
             console.log('Constructor Avenger llamado!');
         }
         getFullname() {
-            return `${this.name} ${this.reaName}`;
+            return `${this.name} ${this.realName}`;
         }
     }
     class Xmen extends Avenger {
@@ -32,12 +32,22 @@
             this.isMutant = isMutant;
             console.log('Constructor Xmen llamado');
         }
+        get fullName() {
+            return `${this.name}- ${this.realName}`;
+        }
+        set fullName(name) {
+            if (name.length < 3) {
+                throw new Error('El nombre debe de ser mayor de 3 letras');
+            }
+            this.name = name;
+        }
         getFullnameDesdeXmen() {
             console.log(super.getFullname());
         }
     }
     const wolverine = new Xmen('Wolverine', 'Logan', true);
-    console.log(wolverine);
-    wolverine.getFullnameDesdeXmen();
+    console.log(wolverine.fullName);
+    wolverine.fullName = 'Noelia';
+    console.log(wolverine.fullName);
 })();
 //# sourceMappingURL=main.js.map
