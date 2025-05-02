2ae4673c7674c6d80b2a7be3fd7417bbe8c0b853
Noe <noelia2000vane@gmail.com>
2025-05-02 00:47:52 -0400

    Forma corta de asignar propiedades

--- Typescript/bases/classes/basica.ts
+++ Typescript/bases/classes/basica.ts
@@ -2,19 +2,29 @@
  class Avenger{
 
     //private significa de que solo voy a tener acceso a esta propiedad dentro  de la clase
-    private name:string;
-    private team: string;
-    public realName: string;
+    // private name:string;
+    // private team: string;
+    // public realName: string;
     static avgAge:number=35;
-    constructor(name:string, team:string, realName?:string){
-        this.name=name;
-        this.team=team;
-        this.realName=realName;
+
+    constructor
+    (
+        private name:string,
+        private team:string, 
+        public realName?:string,
+        // avgAge:number=55,
+    )
+        { 
+            // Avenger.avgAge=avgAge;
+        // this.name=name;
+        // this.team=team;
+        // this.realName=realName;
     }
  }
 
- const antman : Avenger=new Avenger('Antman','Capitan');
+ const antman : Avenger=new Avenger('Antman','Capitan','Scott Lang');
  console.log(antman)
 // console.log(Avenger.avgAge)
+console.log(Avenger.avgAge)
 
 })()
