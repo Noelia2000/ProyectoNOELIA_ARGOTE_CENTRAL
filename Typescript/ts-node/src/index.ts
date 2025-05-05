a2e48b1c071fb8cc644dec5aca531efbf20d3dfa
Noe <noelia2000vane@gmail.com>
2025-05-05 00:34:27 -0400

    Introducción a los Genéricos

--- Typescript/ts-node/src/index.ts
+++ Typescript/ts-node/src/index.ts
@@ -1,19 +1,12 @@
-// import { Hero as SuperHero, Hero2 } from "./classes/hero";
-// import * as HeroClasses from './classes/Hero';
-// import powers from './data/powers';
-import { Hero } from "./classes/Hero";
+import { printObject, genericFunction } from "./generics/generics";
 
-// const Hero=123;
-
-
-// const ironman = new SuperHero('Ironman', 1, 55);
-const ironman = new  Hero('Ironman', 1, 55);
- 
-console.log(ironman);
-console.log(ironman.power);
-
-// console.log(powers)
+// printObject (123);
+// printObject(new Date());
+// printObject({a:queueMicrotask,b:2,c:3});
+// printObject([1,2,3,4,5,6,7,8,9,10]);
+// printObject('Hola Mundo');
 
 
-
-
+console.log(genericFunction(3.141618).toFixed(2))
+console.log(genericFunction('Hola Mundo').toFixed(2))
+console.log(genericFunction(new Date()).toFixed(2))
