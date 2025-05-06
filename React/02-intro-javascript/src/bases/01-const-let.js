d7432ac535a1b8517e416308f6cac2cc07d50c69
Noe <noelia2000vane@gmail.com>
2025-05-05 22:50:32 -0400

    Variables y constantes

---
+++ React/02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,19 @@
+
+
+const nombre='Noelia';
+const apellido='Argote';
+
+let valorDado=5;
+valorDado=4;
+
+console.log(nombre,apellido,valorDado)
+
+// var NO se debe de usar
+if(true){
+
+   const nombre='Peter';
+
+    console.log(nombre)
+}
+
+console.log(valorDado);
