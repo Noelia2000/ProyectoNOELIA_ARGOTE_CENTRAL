4ae450a552c0f334c20d6e85d8c556097aea14fd
Noe <noelia2000vane@gmail.com>
2025-05-07 17:13:29 -0400

    DefaultProps

--- React/03-counter-app-vite/src/FirstApp.jsx
+++ React/03-counter-app-vite/src/FirstApp.jsx
@@ -5,7 +5,8 @@
 // };
 
 import PropTypes from 'prop-types';
-export const FirstApp=({title, subTitle})=>{
+
+export const FirstApp=({title, subTitle, name})=>{
 
 
   //  console.log(props);
@@ -18,6 +19,7 @@ return(
   <h1>{title}</h1>
   {/* <code>{JSON.stringify(newMessage)}</code> */}
   <p> {subTitle}</p>
+  <p> {name}</p>
   
   </>
 )
@@ -26,6 +28,12 @@ return(
 
 FirstApp.propTypes={
   title:PropTypes.string.isRequired,
-  subTitle: PropTypes.number,
+  subTitle: PropTypes.string,
 
+}
+
+FirstApp.defaultProps = {
+  name:'Noelia Argote',
+  subTitle:'No hay sub titulo',
+  title:'No hay titulo',
 }
