1b3dab987552933fda8cb6c1834deda90b795283
Noe <noelia2000vane@gmail.com>
2025-05-07 00:09:47 -0400

    Async - Await

---
+++ React/02-intro-javascript/src/bases/11-async-await.js
@@ -0,0 +1,27 @@
+
+
+// const getImagenPromesa=()=>new Promise(resolve=>resolve('https://ajsknfndkf.com'))
+// getImagenPromesa().then(console.log);
+
+const getImagen=async()=>{
+try{
+    const apiKey='Q9kcKMU8ZP4YdYgTGy3otQbCBkQ5VdGa';
+    const resp=await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+    const {data}=await resp.json();
+    
+    const{url}=data.images.original;
+
+    const img= document.createElement('img');
+    img.src=url;
+     document.body.append(img);
+}catch(error){
+    //manejo del error
+    console.error(error)
+}
+    
+}
+getImagen();
+
+
+
+
