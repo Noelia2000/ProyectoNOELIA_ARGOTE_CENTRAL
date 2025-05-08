432ed2fab8d2425838a8f306411c948b521b108c
Noe <noelia2000vane@gmail.com>
2025-05-07 22:03:29 -0400

    handleSubtract y handleReset

--- React/03-counter-app-vite/src/CounterApp.jsx
+++ React/03-counter-app-vite/src/CounterApp.jsx
@@ -2,6 +2,9 @@ import {useState} from'react';
 import PropTypes  from "prop-types";
 
 export const CounterApp = ({value}) => {
+
+// console.log('render');
+
   const [counter, setCounter]=useState(value);
   const handleAdd=()=>{
     // console.log(event)
@@ -9,13 +12,23 @@ export const CounterApp = ({value}) => {
 
     // setCounter((c)=>c+1)
   }
+
+
+  const handleSubtract=()=> setCounter(counter-1);
+  const handleReset=()=> setCounter(value);
+
+
+
+
+
   return (
     <>
     <h1>CounterApp</h1>
     <h2> {counter} </h2>
-    <button onClick={handleAdd}>
-      +1
-    </button>
+    <button onClick={handleAdd}>+1 </button>
+    <button onClick={handleSubtract}>-1 </button>
+    <button onClick={handleReset}>Reset </button>
+
     </>
   );
 }
