86091464a5192306a0edf32ba6d9e6c7289a978c
Noe <noelia2000vane@gmail.com>
2025-05-09 11:09:38 -0400

    Probar FirstApp

--- React/03-counter-app-vite/tests/FirstApp.test.jsx
+++ React/03-counter-app-vite/tests/FirstApp.test.jsx
@@ -6,6 +6,18 @@ describe('Pruebas en <FirstApp/>', () => {
     test('debe de hacer match con el snapshot', () => {  
 
         const title='Hola, soy Goku';
-        render(<FirstApp title={title}/>)
+        const{container}=render(<FirstApp title={title}/>);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    test('debe de mostrar el titulo en h1', () => {  
+        const title='Hola, soy Goku';
+        const{container, getByText}= render(<FirstApp title={title}/>);
+
+        expect (getByText(title)).toBeTruthy( );
+
+        const h1 =container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title)
     });
 });
