108d5c8d9b713502ba4432ba8a0e09d78b017f6f
Noe <noelia2000vane@gmail.com>
2025-05-13 05:55:00 -0400

    useEffect

--- React/04-gif-expert-app/src/components/GifGrid.jsx
+++ React/04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,3 +1,4 @@
+import { useState,useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 
@@ -5,17 +6,23 @@ import { getGifs } from "../helpers/getGifs";
   
 
     export const GifGrid=({category})=>{
-        
-        
+        const [counter,setCounter]=useState(10);
+
+        useEffect(()=>{
+
+
         getGifs(category);
 
 
+        },[])
+        
+
   return (
     <>
-    
 
     <h3>{category}</h3>
-    
+    <h5>{counter}</h5>
+    <button onClick={()=>setCounter(counter+1)}>+1</button>
     </>
   )
     }
