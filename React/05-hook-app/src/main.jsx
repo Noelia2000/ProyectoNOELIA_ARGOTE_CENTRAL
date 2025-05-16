5ef118e97d3b28e8fed7af8fef5b05cf718f603d
Noe <noelia2000vane@gmail.com>
2025-05-16 17:05:29 -0400

    Memo - Método de React

--- React/05-hook-app/src/main.jsx
+++ React/05-hook-app/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { Memorize } from './06-memos/Memorize';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
-import { Layout } from './05-useLayoutEffect/Layout';
+// import { Layout } from './05-useLayoutEffect/Layout';
+
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { HooksApp } from './HooksApp';
@@ -13,6 +15,6 @@ import { Layout } from './05-useLayoutEffect/Layout';
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <Layout/>
+    <Memorize/>
   //  </StrictMode>,
 )
