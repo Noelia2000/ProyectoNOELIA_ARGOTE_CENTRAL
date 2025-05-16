bab66171409557e608cc1cbfcb7456988d00d638
Noe <noelia2000vane@gmail.com>
2025-05-16 00:39:20 -0400

    Formulario con custom Hook

--- React/05-hook-app/src/main.jsx
+++ React/05-hook-app/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { SimpleForm } from './02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { HooksApp } from './HooksApp';
@@ -9,6 +10,6 @@ import { SimpleForm } from './02-useEffect/SimpleForm';
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <SimpleForm/>
+    <FormWithCustomHook/>
   // </StrictMode>,
 )
