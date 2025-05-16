a9ba7aa1eba3b86e602c100976fe8a7c195a9bfe
Noe <noelia2000vane@gmail.com>
2025-05-15 22:59:30 -0400

    useEffect unmount - Cleanup

--- React/05-hook-app/src/02-useEffect/SimpleForm.jsx
+++ React/05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Message } from "./Message";
 
 export const SimpleForm = () => {
 
@@ -23,17 +24,17 @@ export const SimpleForm = () => {
 
 
     useEffect(() => {
-        console.log('useEffect called');
+        // console.log('useEffect called');
     }, []);
 
 
     useEffect(() => {
-        console.log('formState changed');
+        // console.log('formState changed');
     }, [formState]);
 
 
     useEffect(() => {
-        console.log('email changed');
+        // console.log('email changed');
     }, [email]);
 
 
@@ -62,6 +63,11 @@ export const SimpleForm = () => {
                 value={email}
                 onChange={onInputChange}
             />
+
+           {
+            (username==='strider2') && <Message/>
+           } 
+
         </>
     )
 }
