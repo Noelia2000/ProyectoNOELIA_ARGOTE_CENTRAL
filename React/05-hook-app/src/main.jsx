936fe64f381b1a17cce6f553f07ac71e9e9c1a9f
Noe <noelia2000vane@gmail.com>
2025-05-16 16:41:48 -0400

    useLayoutEffect

--- React/05-hook-app/src/main.jsx
+++ React/05-hook-app/src/main.jsx
@@ -4,14 +4,15 @@ import './index.css';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-import { FocusScreen } from './04-useRef/FocusScreen';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+import { Layout } from './05-useLayoutEffect/Layout';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { HooksApp } from './HooksApp';
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <FocusScreen/>
-   </StrictMode>,
+  // <StrictMode>
+    <Layout/>
+  //  </StrictMode>,
 )
