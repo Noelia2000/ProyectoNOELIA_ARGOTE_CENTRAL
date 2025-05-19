69229aa1afadcde86581d964c5a44e7109f8a787
Noe <noelia2000vane@gmail.com>
2025-05-19 12:06:30 -0400

    Resolución de la tarea - TodoApp

--- React/05-hook-app/src/08-useReducer/TodoApp.jsx
+++ React/05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,5 +1,7 @@
 import { useReducer } from "react";
 import { todoReducer } from "./TodoReducer";
+import { TodoList } from "./TodoList";
+import { TodoAdd } from "./TodoAdd";
 
 
 const initialState = [
@@ -12,7 +14,7 @@ const initialState = [
 
     {
         id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del alma',
+        description: 'Recolectar la piedra del tiempo',
         done: false,
     },
 
@@ -38,22 +40,9 @@ export const TodoApp = () => {
 
             <div className="row">
                 <div className="col-7">
-                    {/* {   TodoList} */}
-                    <ul className="list-group ">
-                        {
-                            todos.map(todo => (
-                                <li key={todo.id} className="list-group-item d-flex justify-content-between">
-                                    <span className="align-self-center">Item 1</span>
-                                    <button className="btn btn-danger">Borrar</button>
 
-                                </li>
+                    <TodoList todos={todos} />
 
-
-                            ))
-                        }
-
-                    </ul>
-                    {/* { Fin TodoList} */}
 
                 </div>
 
@@ -61,28 +50,8 @@ export const TodoApp = () => {
                 <div className="col-5">
                     <h4>Agregar TODO</h4>
                     <hr />
-                    {/* { TodoAdd onNewTodo(todo)} */}
-                    {/* {{id:new Date().getDate.apply.apply, description:'',done:false}} */}
-                    <form>
-
-                        <input
-                            type="text"
-                            placeholder="¿Qué hay que hacer?"
-                            className="form-control"
-
-                        />
-
-                        <button
-
-                            type="submit"
-                            className="btn btn-outline-primary mt-1 "
-                        >
-                            Agregar
-                        </button>
-
-                    </form>
-
-
+                  
+                    <TodoAdd onNewTodo={ handleNewTodo}/>
 
 
                 </div>
