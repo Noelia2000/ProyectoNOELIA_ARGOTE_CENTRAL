92821dff6d2d0840e50d7b105c591a0ac36cf115
Noe <noelia2000vane@gmail.com>
2025-05-21 13:01:09 -0400

    useContext

--- React/05-hook-app/src/09-useContext/context/UserProvider.jsx
+++ React/05-hook-app/src/09-useContext/context/UserProvider.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user={
-    id: 123,
-    name: 'Noelia Argote',
-    email: 'Noelia2000@gmail.com'
-}
+// const user={
+//     id: 123,
+//     name: 'Noelia Argote',
+//     email: 'Noelia2000@gmail.com'
+// }
 export const UserProvider = ({ children }) => {
+
+    const [user, setUser] = useState();
+
     return (
-        <UserContext.Provider value={{hola:'Mundo', user:user}}>
+        // <UserContext.Provider value={{hola:'Mundo', user:user}}>
+        <UserContext.Provider value={{ user, setUser }}>
+
             {children}
 
         </UserContext.Provider>
