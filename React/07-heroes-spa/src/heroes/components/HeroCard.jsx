2d6afcc087aceb7c4fe27227fe1c4bfacaa9ae3e
Noe <noelia2000vane@gmail.com>
2025-05-21 23:50:58 -0400

    Tarjeta del Héroe - parte 2

--- React/07-heroes-spa/src/heroes/components/HeroCard.jsx
+++ React/07-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+
+//     if (alter_ego === characters) return (<></>);
+//     return <p> {characters}</p>
+return ( alter_ego === characters)
+? <></>
+:<p> {characters}</p>;
+}
 
 export const HeroCard = ({
     id,
@@ -9,6 +19,8 @@ export const HeroCard = ({
 }) => {
 
     const heroImageUrl = `/assets/heroes/${id}.jpg`;
+
+    // const charactesByHero=(<p>{ characters}</p>)
 
     return (
         <div className="col">
@@ -24,10 +36,23 @@ export const HeroCard = ({
                     <div className="col-8">
                         <div className="card-body">
 
-                            <h5 className="card-title">{ superhero}</h5>
-                            <p className=" card-text">{ alter_ego}</p>
+                            <h5 className="card-title">{superhero}</h5>
+                            <p className=" card-text">{alter_ego}</p>
+                            {/* {
 
-                            <p>{characters}</p>
+                                (alter_ego !== characters) && charactesByHero
+                            } */}
+                            <CharactersByHero characters={characters} alter_ego={alter_ego} />
+
+                            <p className="card-text">
+                                <small className="text-muted">{first_appearance}</small>
+                            </p>
+
+
+                            <Link to={`/hero/${id}`}>
+                                Mas...
+                            </Link>
+
                         </div>
                     </div>
 
