2208be3189deef8647c324ccd248a47b901c0304
Noe <noelia2000vane@gmail.com>
2025-05-22 23:10:03 -0400

    Rutas públicas

--- React/07-heroes-spa/src/router/AppRouter.jsx
+++ React/07-heroes-spa/src/router/AppRouter.jsx
@@ -5,6 +5,7 @@ import { LoginPage } from "../auth"
 
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes"
 import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute"
 
 export const AppRouter = () => {
   return (
@@ -14,15 +15,19 @@ export const AppRouter = () => {
 
       <Routes>
 
-
-        <Route path="login" element={<LoginPage />} />
+        <Route path="/login" element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        }
+        />
 
         <Route path="/*" element={
           <PrivateRoute>
             <HeroesRoutes />
           </PrivateRoute>
         } />
-
+        {/* <Route path="login" element={<LoginPage />} /> */}
         {/* <Route path="/*" element={<HeroesRoutes />} /> */}
 
 
