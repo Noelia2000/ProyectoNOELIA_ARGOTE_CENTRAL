91467e1def302806b324bdcbc74e4db107239daa
Noe <noelia2000vane@gmail.com>
2025-05-22 23:26:14 -0400

    Recordar la última página visitada

--- React/07-heroes-spa/src/auth/pages/LoginPage.jsx
+++ React/07-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,36 +1,33 @@
-import { useContext } from "react";
 import { useNavigate } from "react-router-dom"
 import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+
 
 export const LoginPage = () => {
-
-  const { login } = useContext(AuthContext)
+  
+  const {login} = useContext(AuthContext)
   const navigate = useNavigate();
 
-  const onLogin = () => {
+  const  onLogin = () =>{
 
-    login('Noelia Argote');
+    const lastPath = localStorage.getItem('lastPath') || '/'
 
-    navigate('/', {
-      replace: true
-    });
+    login('Noelia Argote')
 
-
-
-  }
+    navigate( lastPath,{
+      replace: true,
+    })
+  }  
+  
   return (
-    <div className="container mt-5">
+    <div className="container">
       <h1>Login</h1>
-      <hr />
+      <hr/>
 
-      <button
-        className="btn btn-primary"
-        onClick={onLogin}
-
-      >
+      <button className="btn btn-primary" onClick={onLogin}>
         Login
-
       </button>
+
     </div>
   )
 }
