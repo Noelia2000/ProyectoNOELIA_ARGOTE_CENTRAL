55e5796f46cfeaccc006b79c97ee23d40a1a4251
Noe <noelia2000vane@gmail.com>
2025-05-26 18:44:02 -0400

    Tarea - decrement e incrementBy

--- React/09-toolkit-redux/src/App.jsx
+++ React/09-toolkit-redux/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { increment } from './store/slices/counter'
+import { increment,decrement, incrementBy } from './store/slices/counter'
 
 function App() {
 
@@ -20,13 +20,20 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>count is {counter}</h1>
       <div className="card">
-        <button onClick={() => {dispatch(increment())}}>
-          count is {counter}
+        <button onClick={() => { dispatch(increment()) }}>
+          Increment
         </button>
 
+        <button onClick={() => { dispatch(decrement()) }}>
+            Decrement
+        </button>
+      
       </div>
+       <button onClick={() => {dispatch(incrementBy(2))}}>
+         Increment by 2
+        </button>
 
     </>
   )
--- React/09-toolkit-redux/src/store/slices/counter/counterSlice.js
+++ React/09-toolkit-redux/src/store/slices/counter/counterSlice.js
@@ -4,7 +4,8 @@ import { createSlice } from '@reduxjs/toolkit'
 export const counterSlice = createSlice({
   name: 'counter',
   initialState:{
-    counter: 10
+    counter: 10,
+    times:0
   },
   reducers: {
     increment: (state) => {
@@ -13,10 +14,17 @@ export const counterSlice = createSlice({
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
     state.counter+=1;
+  
     },
-    
-  },
+     decrement: (state) => {
+      state.counter -= 1;
+    },
+    incrementBy: (state) => {
+      state.counter += 2;
+
+  
+}}
 })
 
 // Action creators are generated for each case reducer function
-export const { increment} = counterSlice.actions;
+export const { increment, decrement, incrementBy} = counterSlice.actions;
