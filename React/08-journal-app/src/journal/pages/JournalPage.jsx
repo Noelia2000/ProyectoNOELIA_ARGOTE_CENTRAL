c83ba2505c302d587a4f5d9100759ab6a12b848f
Noe <noelia2000vane@gmail.com>
2025-05-25 23:12:02 -0400

    NoteView

--- React/08-journal-app/src/journal/pages/JournalPage.jsx
+++ React/08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,15 +1,15 @@
 
 
 import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView } from "../views"
+import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
    {/* <Typography>Amet officia sunt ullamco aute aliqua dolore aliquip tempor occaecat sit ullamco ad ad.</Typography> */}
-  
-  <NothingSelectedView/>
-  {/* NoteView */}
+{/*   
+  <NothingSelectedView/> */}
+  <NoteView/>
     </JournalLayout>
   )
 }
---
+++ React/08-journal-app/src/journal/views/NoteView.jsx
@@ -0,0 +1,45 @@
+import { SaveOutlined } from "@mui/icons-material"
+import { Button, Grid, TextField, Typography } from "@mui/material"
+import { ImageGallery } from "../components"
+
+export const NoteView = () => {
+    return (
+         <Grid container direction='column' spacing={2}>
+        <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{ mb: 1 }}>
+            <Grid item>
+                <Typography fontSize={39} fontWeight='ligh'>25 de Mayo, 2025</Typography>
+            </Grid>
+            <Grid item>
+                <Button color="primary" sx={{ padding: 2 }}>
+                    <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+                    Guardar
+                </Button>
+            </Grid>
+            </Grid>
+          
+           
+                <Grid item>
+                    <TextField
+                        type=" text"
+                        variant="filled"
+                        fullWidth
+                        placeholder="Ingrese un titulo"
+                        label="Titulo"
+                        sx={{ border: 'none', mb: 1 }}
+                    />
+                     <TextField
+                        type=" text"
+                        variant="filled"
+                        fullWidth
+                        multiline
+                        placeholder="que sucedio en el dia de hoy?"
+                        minRows={5}
+                      
+                    />
+
+                </Grid>
+                 {/* Image gallery */}
+              <ImageGallery/>
+        </Grid>
+    )
+}
