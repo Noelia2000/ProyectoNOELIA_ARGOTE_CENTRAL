4c770f3b7a2570e0c7e53ad4089ef1621fc38959
Noe <noelia2000vane@gmail.com>
2025-05-29 23:36:45 -0400

    Custom Hook para autenticación

--- React/08-journal-app/src/router/AppRouter.jsx
+++ React/08-journal-app/src/router/AppRouter.jsx
@@ -1,30 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
-import { useDispatch, useSelector } from "react-redux"
 import { CheckingAuth } from "../ui/components/CheckingAuth"
-import { onAuthStateChanged } from "firebase/auth"
-import { FirebaseAuth } from "../firebase/config"
-import { useEffect } from "react"
-import { login, logout } from "../store/auth"
+import { useCheckAuth } from "../hooks"
 
 export const AppRouter = () => {
 
-    const { status } = useSelector(state => state.auth);
-
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        onAuthStateChanged(FirebaseAuth, async (user) => {
-            if (!user) return dispatch(logout());
-
-            const { uid, email, displayName, photoURL } = user;
-            dispatch(login({ uid, email, displayName, photoURL }));
-            console.log(user);
-        })
-
-    }, []);
-
+    
+const status= useCheckAuth();
 
 
 
