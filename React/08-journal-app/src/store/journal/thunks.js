dfed5d8df301e0a1e0228a2af00b7e939e2a5e30
Noe <noelia2000vane@gmail.com>
2025-06-02 15:31:05 -0400

    Preparar la base de datos - CloudFirestore

---
+++ React/08-journal-app/src/store/journal/thunks.js
@@ -0,0 +1,12 @@
+
+export const startNewNote = ()=>{
+    return async ( dispatch)=>{
+        //uid
+
+        const newNote={
+            title:'',
+            body:'',
+            date: new Date().getTime(),
+        }
+    }
+}
