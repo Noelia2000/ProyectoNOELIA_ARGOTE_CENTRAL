0852db60e4e51445fcdcac1a94c7b11b2e124451
Noe <noelia2000vane@gmail.com>
2025-06-04 23:39:33 -0400

    Cargar notas de Firestore

--- React/08-journal-app/src/hooks/useCheckAuth.js
+++ React/08-journal-app/src/hooks/useCheckAuth.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout } from "../store/auth";
 import { FirebaseAuth } from "../firebase/config";
+import { startLoadingNotes } from "../store/journal";
 
 export const useCheckAuth = () => {
     const { status } = useSelector(state => state.auth);
@@ -15,7 +16,8 @@ export const useCheckAuth = () => {
 
             const { uid, email, displayName, photoURL } = user;
             dispatch(login({ uid, email, displayName, photoURL }));
-            console.log(user);
+            dispatch( startLoadingNotes());
+         
         })
 
     }, []);
