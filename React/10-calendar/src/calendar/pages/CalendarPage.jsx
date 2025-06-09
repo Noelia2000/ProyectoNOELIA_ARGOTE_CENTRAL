b102c6ce2502b0d1c949fc1be5758ca8e6766b10
Noe <noelia2000vane@gmail.com>
2025-06-09 18:34:37 -0400

    Creando un modal sobre el calendario

--- React/10-calendar/src/calendar/pages/CalendarPage.jsx
+++ React/10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -3,7 +3,7 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 
 import { addHours } from 'date-fns'
-import { CalendarEvent, Navbar } from "../";
+import { CalendarEvent, CalendarModal, Navbar } from "../";
 import { localizer,getMessagesES } from '../../helpers';
 import { useState } from 'react';
 
@@ -79,6 +79,7 @@ export const CalendarPage = () => {
       onSelectEvent={onSelect}
       onView={onViewChanged}
     />
+    <CalendarModal/>
 
     </>
   )
