14bc98beaeb403091eae3b2c3ff7611857911dd2
Noe <noelia2000vane@gmail.com>
2025-08-05 13:37:15 -0400

    Custom Middlewares

--- React/10-calendar-backend/controllers/auth.js
+++ React/10-calendar-backend/controllers/auth.js
@@ -7,15 +7,6 @@ const crearUsuario =(req, res= response)=>{
 
    const {name, email, password}= req.body;
 
-   //manejo de errores
-   const errors = validationResult(req);
-   if ( !errors.isEmpty()){
-    return res.status(400).json({
-        ok: false,
-        errors:errors.mapped()
-    });
-   }
-  
 
 res.status(201).json({
     ok: true,
@@ -28,15 +19,7 @@ res.status(201).json({
 
 const loginUsuario=(req, res= response)=>{
 
-    //manejo de errores
-   const errors = validationResult(req);
-   if ( !errors.isEmpty()){
-    return res.status(400).json({
-        ok: false,
-        errors:errors.mapped()
-    });
-   }
-  
+ 
  const { email, password}= req.body;
 res.json({
     ok: true,
--- React/10-calendar-backend/routes/auth.js
+++ React/10-calendar-backend/routes/auth.js
@@ -6,7 +6,7 @@ host + /api/auth
 const{Router}= require('express');
 const{ check}= require('express-validator');
 const router=Router();
-
+const{validarCampos}= require ('../middlewares/validar-campos');
 const {crearUsuario,loginUsuario,revalidarToken}= require('../controllers/auth');
 
 router.post(
@@ -15,6 +15,7 @@ router.post(
         check('name','El nombre es obligatorio').not().isEmpty(),
         check('email','El email es obligatorio').isEmail(),
         check('password','El password debe de ser de 6 caracteres').isLength({min:6}),
+        validarCampos
         
      ], 
      crearUsuario
@@ -24,6 +25,7 @@ router.post(
     [
         check('name','El nombre es obligatorio').not().isEmpty(),
         check('email','El email es obligatorio').isEmail(),
+        validarCampos
     ],
     loginUsuario);
 router.get('/renew',revalidarToken);
